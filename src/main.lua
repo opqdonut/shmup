bd4a8046aaf8ddbf98e9595f9da8e6757ec9d72c
Joel Kaasinen <opqdonut@endo.(none)>
2010-01-28 20:27:03 +0200

    enemies

--- src/main.lua
+++ src/main.lua
@@ -15,7 +15,7 @@ function Thing:new(o)
 end
 
 function Thing:draw()
-   love.graphics.draw(self.sprite, self.x, self.y)
+   love.graphics.draw(self.sprite, self.x-self.ox, self.y-self.oy)
 end
 
 function Thing:update(dt)
@@ -28,32 +28,92 @@ function Thing:accel(dvx, dvy)
    self.vy = self.vy + dvy
 end
 
+function Thing:move(dx, dy)
+   self.x = self.x + dx
+   self.y = self.y + dy
+end
+
 ship = Thing:new {sprite = love.graphics.newImage("ship.png"),
+		  ox = 32, oy = 32,
 	          x = 100.0, y = 100.0}
 
-things = {ship}
+function ship:update(dt)
+
+   Thing.update(self, dt)
+
+   local w = love.graphics.getWidth()
+   local h = love.graphics.getHeight()
+
+   if self.x < 0 then self.x = 0 self.vx = 0 end
+   if self.y < 0 then self.y = 0 self.vy = 0 end
+   if self.x > w then self.x = w self.vx = 0 end
+   if self.y > h then self.y = h self.vy = 0 end
+end
 
 --
 
-binds = {
-   s = function () ship:accel(0,10) end,
-   d = function () ship:accel(10,0) end,
-   w = function () ship:accel(0,-10) end,
-   a = function () ship:accel(-10,0) end,
+things = {ship}
 
-   down = function () ship:accel(0,10) end,
-   right = function () ship:accel(10,0) end,
-   up = function () ship:accel(0,-10) end,
-   left = function () ship:accel(-10,0) end,
+function removething(thing)
+   for i, v in ipairs(things) do
+      if rawequal(v,thing) then
+	 table.remove(things,i)
+	 return
+      end
+   end
+end
+
+function newenemy()
+   
+   if math.random() < 0.99 then
+      return
+   end
+
+   local enemy = Thing:new {
+      sprite = love.graphics.newImage("enemy1.png"),
+      ox = 32, oy = 32,
+      y = 0, x = math.random(love.graphics.getWidth()),
+      vx = 0, vy = math.random(100)}
+
+   function enemy:update(dt)
+      Thing.update(self, dt)
+      if self.y > love.graphics.getHeight() then
+	 removething(self)
+      end
+   end
+
+   table.insert(things,enemy)
+end
+
+updatehooks = {newenemy, function() print(table.maxn(things)) end}
+
+--
+
+speed = 3
+
+binds = {
+   s = function () ship:move(0,speed) end,
+   d = function () ship:move(speed,0) end,
+   w = function () ship:move(0,-speed) end,
+   a = function () ship:move(-speed,0) end,
+
+   down =  function () ship:move(0,speed) end,
+   right = function () ship:move(speed,0) end,
+   up =    function () ship:move(0,-speed) end,
+   left =  function () ship:move(-speed,0) end,
 }
 
 --
 
 function love.update(dt)
 
+   for _, hook in ipairs(updatehooks) do
+      hook()
+   end
+
    for key, action in pairs(binds) do
       if love.keyboard.isDown(key) then
-	 print("event", key)
+	 --print("event", key)
 	 action()
       end
    end
